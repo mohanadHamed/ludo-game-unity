3587aba4eb00dfd15af41e6c454c895b831cbe38
1810msa <mohanad_subhi@yahoo.com>
2023-11-02 08:52:14 +0300

    Add tile grid parameters

--- LudoGame/Assets/Scripts/UI/Tiles/TilesGrid.cs
+++ LudoGame/Assets/Scripts/UI/Tiles/TilesGrid.cs
@@ -1,18 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.UI.Tiles
 {
+    public enum PlayerColorOption
+    {
+        Yellow,
+        Green,
+        Blue,
+        Red
+    }
+
     public class TilesGrid : MonoBehaviour
     {
+        [SerializeField] private PlayerColorOption _playerColorOption;
+
+
+        [SerializeField] private GameObject _tilePrefab;
+
         private const int GridColumns = 3;
 
         private const int GridRows = 6;
 
         private readonly Color _defaultColor = Color.white;
-        
-        [SerializeField] private Color _gridColor;
 
-        [SerializeField] private GameObject _tilePrefab;
+        private readonly TileComponent[,] _gridTiles = new TileComponent[GridRows, GridColumns];
+
+        private readonly Dictionary<PlayerColorOption, Color> _gridColorMap = new() {
+            { PlayerColorOption.Yellow, new Color(1f, 0.949f, 0f)},
+            { PlayerColorOption.Green, new Color(0.133f, 0.694f, 0.298f)},
+            { PlayerColorOption.Blue, new Color(0f, 0.635f, 0.91f)},
+            { PlayerColorOption.Red, new Color(0.929f, 0.11f, 0.141f)},
+        };
 
         private void Start()
         {
@@ -20,11 +39,9 @@ namespace Assets.Scripts.UI.Tiles
             {
                 for (var col = 0; col < GridColumns; col++)
                 {
-                    var usedColor = (row > 0 && col == 1) || (row == 1 && col == 2)
-                        ? _gridColor
-                        : _defaultColor;
+                    var usedColor = (row > 0 && col == 1) || (row == 1 && col == 2) ? _gridColorMap[_playerColorOption] : _defaultColor;
 
-                    TileFactory.CreateTile(_tilePrefab, usedColor, transform);
+                    _gridTiles[row, col] = TileFactory.CreateTile(_tilePrefab, usedColor, transform);
                 }
             }
         }
